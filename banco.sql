33b6f7219cfdc691c2258cef9c47038dfcced0fd
Edward Felipe Viana <112318294+EdFelipe51@users.noreply.github.com>
2023-03-05 22:27:22 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -4,5 +4,9 @@ CREATE TABLE dados (
     Sobrenome varchar(50),
     Endereco varchar(150),
     Cidade varchar(50),
+    UF varchar (50),
+    Telefone varchar (50),
+    Mae (50),
+    Pai (50),
     Host varchar(50)
 );
